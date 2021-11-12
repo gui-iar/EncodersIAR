dde34aeb330503a992048bab4b5ecbb3103fb38a
fernandoph@gmail.com <fernandoph@gmail.com>
2021-11-12 01:03:56 -0300

    punto flotante en el .ino, sincronismo en el .c

--- ino/encoders.ino
+++ ino/encoders.ino
@@ -1,8 +1,10 @@
+#define N_DECIMAL_POINTS_PRECISION (100) // n = 2. Dos decimales
+
 const int PIN_CLOCK_1 = 7;
-const int PIN_DATA_1 = 6;
+const int PIN_DATA_1  = 6;
 
 const int PIN_CLOCK_2 = 5;
-const int PIN_DATA_2 = 4;
+const int PIN_DATA_2  = 4;
 
 unsigned int GrayToBinary(unsigned int num)
 {
@@ -17,34 +19,39 @@ unsigned int GrayToBinary(unsigned int num)
 void setup() {
   Serial.begin(57600);
 
-  pinMode(PIN_CLOCK_1, OUTPUT);
-  pinMode(PIN_DATA_1, INPUT);
-  pinMode(PIN_CLOCK_2, OUTPUT);
-  pinMode(PIN_DATA_2, INPUT_PULLUP);
-  
-//  digitalWrite(PIN_CLOCK_1, HIGH);
-//  digitalWrite(PIN_CLOCK_2, HIGH);
+  pinMode (PIN_CLOCK_1, OUTPUT);
+  pinMode (PIN_DATA_1,  INPUT);
+  pinMode (PIN_CLOCK_2, OUTPUT);
+  pinMode (PIN_DATA_2 , INPUT_PULLUP);
+}
 
-  }
-
-
-//byte stream[16];
 void loop() {
 
-  int enco_dly = 0.25;
-  int pos_1 = 0;
-  int pos_2 = 0;  
-  int bits = 13;
+  int enco_dly  = 0.25;
+
+  int pos_1     = 0;
+  int pos_2     = 0;  
+  
+  int bits      = 13;
+  
   float grado_1 = 0.0;
   float grado_2 = 0.0;
-
-  for (int i=0; i<6; i++) {
-    digitalWrite(PIN_CLOCK_1, HIGH);
-    digitalWrite(PIN_CLOCK_2, HIGH);
+  int   grado_1_int;
+  int   grado_1_dec;
+  int   grado_2_int;
+  int   grado_2_dec;
+  
+  char buffer[40];
+  
+  for (int i = 0; i < 6; i++) 
+  {
+    digitalWrite (PIN_CLOCK_1, HIGH);
+    digitalWrite (PIN_CLOCK_2, HIGH);
     delay(enco_dly);
   }
 
-  for (int i=0; i<bits; i++) {
+  for (int i = 0; i < bits; i++) 
+  {
     digitalWrite(PIN_CLOCK_1, LOW);
     digitalWrite(PIN_CLOCK_2, LOW);
     delay(enco_dly);
@@ -53,13 +60,15 @@ void loop() {
     digitalWrite(PIN_CLOCK_2, HIGH);
     delay(enco_dly/2);
   
-    pos_1= pos_1 | digitalRead(PIN_DATA_1);
-    if(i<bits-1) pos_1 = pos_1 << 1;
-    pos_2= pos_2 | digitalRead(PIN_DATA_2);
-    if(i<bits-1) pos_2 = pos_2 << 1;
+    pos_1 = pos_1 | digitalRead(PIN_DATA_1);
+    if (i < bits - 1) pos_1 = pos_1 << 1;
+
+    pos_2 = pos_2 | digitalRead(PIN_DATA_2);
+    if ( i < bits - 1) pos_2 = pos_2 << 1;
   }
 
-  for (int i=0; i<6; i++) {
+  for (int i = 0; i < 6; i++) 
+  {
     digitalWrite(PIN_CLOCK_1, HIGH);
     digitalWrite(PIN_CLOCK_2, HIGH);
     delay(enco_dly);
@@ -67,14 +76,18 @@ void loop() {
 
   grado_1= (float(GrayToBinary(pos_1))*360.0)/(pow(2,13));
   grado_2= (float(GrayToBinary(pos_2))*360.0)/(pow(2,13));
+
+  grado_1_int = (int) grado_1;
+  grado_1_dec = ((int)(grado_1*N_DECIMAL_POINTS_PRECISION)%N_DECIMAL_POINTS_PRECISION);
+
+  grado_2_int = (int) grado_2;
+  grado_2_dec = ((int)(grado_2*N_DECIMAL_POINTS_PRECISION)%N_DECIMAL_POINTS_PRECISION);
   
   delay(20);
-  Serial.print("AR_ANG,");
-  Serial.print(pos_1);
-  Serial.print(",");
-  Serial.print((grado_1));
-  Serial.print(",DEC_ANG,");
-  Serial.print(pos_2);
-  Serial.print(",");
-  Serial.println((grado_2));
+
+  sprintf(buffer, "AR_ANG,%04d,%03d.%02d,DEC_ANG,%04d,%03d.%02d\r\n", 
+          pos_1, grado_1_int, grado_1_dec, pos_2, grado_2_int,grado_2_dec);
+
+  Serial.write(buffer);
 }
+
